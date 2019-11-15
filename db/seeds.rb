2bc6d1e6e06f176f56555fe2cb2876c10c8bf1a9
NYHolzer <nissan.holzer@gmail.com>
2019-11-14 22:56:32 -0500

    added another user in the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(name: "Nissan", username: "NYHolzer", password: "password", work_email:"nholzer@scharfindustries.com", gmail_email: "nissan.holzer@gmail.com")
+User.create(name: "Daniella", username: "DFMar", password: "password", work_email:"nholzer@scharfindustries.com", gmail_email: "dfmar18@gmail.com")
 Project.create(title: "Project X", recurrence: "1x", description: "Project X will work on increasing revenue through customer relationships", deadline: "10/31/2019")
 Project.create(title: "Project Z", recurrence: "Monthly", description: "Project Z mission is to maintain account health through monitoring", deadline: "no deadline")
 ProjectAssignment.create(project_id: "1", user_id: "1")
