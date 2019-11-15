4bcfcbae15bf97026246ae2ae62b610acd91b2a1
NYHolzer <nissan.holzer@gmail.com>
2019-11-14 22:56:12 -0500

    moved resources projects into the api v1 route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :projects
+  
   # Want API domain to be projask.com/api/v1/resource
   namespace :api do
     namespace :v1 do 
       resources :users
+      resources :projects
     end
   end
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
