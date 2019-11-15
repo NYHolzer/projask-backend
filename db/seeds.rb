e97ed32fe6564da5ac021c4af88419e9eeaa17e2
NYHolzer <nissan.holzer@gmail.com>
2019-11-14 23:13:37 -0500

    deleted schema and reran schema with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,10 @@
 
 User.create(name: "Nissan", username: "NYHolzer", password: "password", work_email:"nholzer@scharfindustries.com", gmail_email: "nissan.holzer@gmail.com")
 User.create(name: "Daniella", username: "DFMar", password: "password", work_email:"nholzer@scharfindustries.com", gmail_email: "dfmar18@gmail.com")
-Project.create(title: "Project X", recurrence: "1x", description: "Project X will work on increasing revenue through customer relationships", deadline: "10/31/2019")
-Project.create(title: "Project Z", recurrence: "Monthly", description: "Project Z mission is to maintain account health through monitoring", deadline: "no deadline")
+Project.create(title: "Project X", recurrence: "1x", description: "Project X will work on increasing revenue through customer relationships", deadline: "10/31/2019", assigned_by: "Daniella")
+Project.create(title: "Project Z", recurrence: "Monthly", description: "Project Z mission is to maintain account health through monitoring", deadline: "no deadline", assigned_by: "Nissan")
+Project.create(title: "Project L", recurrence: "Weekly", description: "Project L involves spreading the Love with some weekly prizes", deadline: "no deadline", assigned_by: "Nissan")
 ProjectAssignment.create(project_id: "1", user_id: "1")
 ProjectAssignment.create(project_id: "2", user_id: "1")
+ProjectAssignment.create(project_id: "2", user_id: "2")
+ProjectAssignment.create(project_id: "3", user_id: "2")
