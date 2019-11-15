3bdc08d2be9a0ded75540518e3afe9ebd636c642
NYHolzer <nissan.holzer@gmail.com>
2019-11-15 00:19:28 -0500

    added project_id to tasks so can relate projects with task

--- db/migrate/20191115050805_create_tasks.rb
+++ db/migrate/20191115050805_create_tasks.rb
@@ -6,7 +6,8 @@ class CreateTasks < ActiveRecord::Migration[5.2]
       t.string :assigned_by
       t.string :status_id
       t.string :dependent_task_id
-
+      t.string :project_id
+      
       t.timestamps
     end
   end
