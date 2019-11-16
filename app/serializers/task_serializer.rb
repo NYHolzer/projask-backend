23f12bca71884acadf98d70453631fd436750abc
NYHolzer <nissan.holzer@gmail.com>
2019-11-16 18:40:25 -0500

    Added relationships to Task Serializer

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -2,4 +2,7 @@ class TaskSerializer
   include FastJsonapi::ObjectSerializer
   attributes :title, :description, :assigned_by, :status_id, :dependent_task_id, :project_id, :created_at
 
+  belongs_to :project
+  has_many :task_assignments
+  has_many :users, through: :task_assignments
 end
