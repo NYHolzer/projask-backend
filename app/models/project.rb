dc9472b24a0ae0a3bba3c838837dfbe3928bef1a
NYHolzer <nissan.holzer@gmail.com>
2019-11-16 18:38:51 -0500

    added comma to has many through relationship

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
     has_many :tasks
     has_many :project_assignments
-    has_many :users through: :project_assignments
+    has_many :users, through: :project_assignments
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
     belongs_to :project
     has_many :task_assignments
-    has_many :users through: :task_assignments
+    has_many :users, through: :task_assignments
 end
