bcf67f0f7c3b9e8330930df1e403e56a70b12698
NYHolzer <nissan.holzer@gmail.com>
2019-11-16 18:39:35 -0500

    added relationships to User Serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,8 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :username, :work_email, :gmail_email
+
+  belongs_to :project
+  has_many :task_assignments
+  has_many :users, through: :task_assignments
 end
