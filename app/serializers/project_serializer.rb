7dd57e153812f1dbc4f1cbcf9031ca28ed63f0f7
NYHolzer <nissan.holzer@gmail.com>
2019-11-16 19:15:51 -0500

    Added relationships to project serializer

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,4 +1,8 @@
 class ProjectSerializer
   include FastJsonapi::ObjectSerializer
   attributes :title, :recurrence, :description, :deadline, :assigned_by
+
+  has_many :tasks
+  has_many :project_assignments
+  has_many :users, through: :project_assignments
 end
