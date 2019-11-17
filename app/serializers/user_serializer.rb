0244661ee4f16fa037e03ffe0a77f530f9da82b9
NYHolzer <nissan.holzer@gmail.com>
2019-11-16 19:21:29 -0500

    edited the relationships in user as copied and paste wrong relationships initially

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,7 +2,11 @@ class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :username, :work_email, :gmail_email
 
-  belongs_to :project
+  has_many :project_assignments
+  has_many :projects, through: :project_assignments
+
   has_many :task_assignments
-  has_many :users, through: :task_assignments
+  has_many :tasks, through: :task_assignments
+
+  has_many :assignee_tasks, foreign_key: :assigned_by, class_name: "Task"
 end
