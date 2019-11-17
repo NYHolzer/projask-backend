0e7f9ede2d1d1e8026d1886949825f605b589058
NYHolzer <nissan.holzer@gmail.com>
2019-11-16 21:30:49 -0500

    added sessions controller with rails g controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::API     
     # mocked version of "being logged in"
     def current_user
         User.first
