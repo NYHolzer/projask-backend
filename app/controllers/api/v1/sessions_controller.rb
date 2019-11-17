0e7e99bbad6e5ec7baef7778eb1c01d35d4d4e2f
NYHolzer <nissan.holzer@gmail.com>
2019-11-17 11:23:20 -0500

    added logout and chagned create and current_user slightlyt

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::SessionsController < ApplicationController
     def create
-        byebug 
         @user = User.find_by(username: params[:session][:username])
     
         if @user && @user.authenticate(params[:session][:password])
           session[:user_id] = @user.id
-          render json: UserSerializer.new(@user), status: :ok
+          render json: @user
         else
           render json: {
             error: "Invalid Credentials"
@@ -15,7 +14,7 @@ class Api::V1::SessionsController < ApplicationController
 
     def get_current_user
         if logged_in
-          render json: UserSerializer.new(current_user)
+          render json: current_user
         else
           render json: {
             error: "No one logged in"
@@ -23,4 +22,10 @@ class Api::V1::SessionsController < ApplicationController
         end
     end
 
+    def destroy 
+        session.clear
+        render json: {
+            notice: "successfully logged out"
+        }
+    end
 end
