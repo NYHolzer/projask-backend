bd163b97bf8c41c4b3fceb656468e50228bc56a6
NYHolzer <nissan.holzer@gmail.com>
2019-11-16 19:22:41 -0500

    added index, show routes, added before action to set task with params id

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,4 +1,22 @@
-class TasksController < ApplicationController
-    belongs_to :user
-    belongs_to :task
+class Api::V1::TasksController < ApplicationController
+    before_action :set_task, only: [:show, :update, :destroy]
+
+    def index 
+        @tasks = Task
+        tasks_json = TaskSerializer.new(@tasks).serialized_json
+        render json: tasks_json
+    end 
+
+    # GET /api/v1/tasks/1
+    def show
+        # render json: @user
+        # json_string = MovieSerializer.new(movie).serialized_json
+        task_json = TaskSerializer.new(@task).serialized_json
+        render json: task_json
+    end
+
+    private
+    def set_task
+        @task = Task.find(params[:id])
+    end
 end
