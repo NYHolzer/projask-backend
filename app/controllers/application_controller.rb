9176521bac1ae86f90ab4e17511dbe5487dcadfe
NYHolzer <nissan.holzer@gmail.com>
2019-11-16 19:36:11 -0500

    removed helpers as not needed in rails. Sinatra whoops.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationController < ActionController::API
-    helpers do 
     # mocked version of "being logged in"
-        def current_user
-            User.first
-        end 
-
-        def logged_in
-            !!current_user
-        end
+    def current_user
+        User.first
     end 
+
+    def logged_in
+        !!current_user
+    end
 end
