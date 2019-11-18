2b9c1db6d9eddf8802cb1dc89da04ab8f8d71d9d
NYHolzer <nissan.holzer@gmail.com>
2019-11-17 19:21:22 -0500

    fixed logout route to delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   post "/api/v1/login", to: "api/v1/sessions#create" 
-  post "/api/v1/logout", to: "api/v1/sessions#destroy" 
+  delete "/api/v1/logout", to: "api/v1/sessions#destroy" 
   get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
   
   # Want API domain to be projask.com/api/v1/resource
