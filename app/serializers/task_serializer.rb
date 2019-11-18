abf5e9f99d8e729b82063a7bb842b30830cc02ce
NYHolzer <nissan.holzer@gmail.com>
2019-11-17 21:38:27 -0500

    no need for task_assignments as relationship in task serializer

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -3,6 +3,5 @@ class TaskSerializer
   attributes :title, :description, :assigned_by, :status_id, :dependent_task_id, :project_id, :created_at
 
   belongs_to :project
-  has_many :task_assignments
   has_many :users, through: :task_assignments
 end
