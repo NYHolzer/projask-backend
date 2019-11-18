b351b8db8f31792b3f01d12f743e81372a8278df
NYHolzer <nissan.holzer@gmail.com>
2019-11-17 21:39:39 -0500

    relationships currently not needed in user serializer. added custom serializer of userTasks

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,11 +2,14 @@ class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :username, :work_email, :gmail_email
 
-  has_many :project_assignments
-  has_many :projects, through: :project_assignments
+  attribute :myTasks do |user|
+    user.tasks
+  end 
+  # has_many :project_assignments
+  # has_many :projects, through: :project_assignments
 
-  has_many :task_assignments
-  has_many :tasks, through: :task_assignments
+  # has_many :task_assignments
+  # has_many :tasks, through: :task_assignments
 
-  has_many :assignee_tasks, foreign_key: :assigned_by, class_name: "Task"
+  # has_many :assignee_tasks, foreign_key: :assigned_by, class_name: "Task"
 end
