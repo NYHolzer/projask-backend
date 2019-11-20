35fa7566f7814664cd7badedebe402aabdf018fa
NYHolzer <nissan.holzer@gmail.com>
2019-11-19 23:28:42 -0500

    added task params

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -19,4 +19,8 @@ class Api::V1::TasksController < ApplicationController
     def set_task
         @task = Task.find(params[:id])
     end
+
+    def task_params
+        params.require(:task).permit(:title, :description)
+    end
 end
