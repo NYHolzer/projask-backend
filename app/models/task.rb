d4aab53b123f2f8c9d5e44752cf9ff6f421786c5
NYHolzer <nissan.holzer@gmail.com>
2019-11-19 23:29:08 -0500

    added some validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,6 @@ class Task < ApplicationRecord
     belongs_to :project
     has_many :task_assignments
     has_many :users, through: :task_assignments
+
+    validates :title, :description, presence: true
 end
